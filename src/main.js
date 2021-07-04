07f6ce06887746ca01aad236e60fec0179e57c36
k-work <kimm.wantingliao@gmail.com>
2021-07-05 00:23:18 +0800

    [fix] recovery v.1

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(VueAxios, axios);
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
